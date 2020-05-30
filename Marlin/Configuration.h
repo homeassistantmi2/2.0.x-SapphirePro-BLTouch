0c5cef15f7bc0d4a6bd5cc40cf67c89e52feabc8
homeassistantmi2 <51816704+homeassistantmi2@users.noreply.github.com>
2020-05-30 22:58:48 +0200

    Driver change

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -687,7 +687,7 @@
  */
 #define X_DRIVER_TYPE  TMC2208_STANDALONE
 #define Y_DRIVER_TYPE  TMC2208_STANDALONE
-#define Z_DRIVER_TYPE  TMC2208_STANDALONE
+#define Z_DRIVER_TYPE  TMC2209_STANDALONE
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
 //#define Z2_DRIVER_TYPE A4988
@@ -1073,12 +1073,12 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true //A$ TMC2208 behavior
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false //A$C TMC2209 behavior
+#define INVERT_E0_DIR true //A$C TMC2209 behavior
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
